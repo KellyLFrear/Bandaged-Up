d9404d5de10525b63d3fb6d360a64e628fee67cf
Kelly Frear <kellylfrear@gmail.com>
2025-02-06 13:47:52 -0800

    Update Doctor.java

--- Doctor.java
+++ Doctor.java
@@ -1,5 +1,6 @@
 import java.util.*;
 
+// Grabs the Patient's ID and Name from the Parent/User Class
 public class Doctor extends User {
     public Doctor(String id, String name) {
         super(id, name);
