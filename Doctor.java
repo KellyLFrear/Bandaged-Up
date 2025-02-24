83b5f3afb5fe4bb7c51899391ec2a5b9ce282f60
Gavin Rosander <74035138+rosanderg913@users.noreply.github.com>
2025-02-24 09:01:19 -0800

    Delete Doctor.java

--- Doctor.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-
-// Grabs the Patient's ID and Name from the Parent/User Class
-public class Doctor extends User {
-    public Doctor(String id, String name) {
-        super(id, name);
-    }
-}
